0ef8f73e8d6ee6d7705e698b6ee8be37130fece1
Dylan Brown <40218657+dylanbr0wn@users.noreply.github.com>
2022-11-02 00:15:20 -0700

    remove unused uninstall script

--- npm/getBinary.js
+++ npm/getBinary.js
@@ -7,21 +7,21 @@ function getPlatform() {
 
   if (type === 'Windows_NT' ){
     if (arch === 'x64') {
-			return 'win64';
+			return {platform:'win64', ext:'.exe'};
 		} else {
-			return 'win32';
+			return {platform:'win32', ext:'.exe'};
 		}
   }
-  if (type === 'Linux' && arch === 'x64') return 'linux';
-  if (type === 'Darwin') return 'macos';
+  if (type === 'Linux' && arch === 'x64') return {platform:'linux64', ext:''};
+  if (type === 'Darwin') return {platform:'darwin', ext:''};
 
   throw new Error(`Unsupported platform: ${type} ${arch}`);
 }
 function getBinary() {
   const platform = getPlatform();
   const version = require('../package.json').version;
-  const url = `https://github.com/dylanbr0wn/mlti/releases/download/v${version}/mlti-${platform}.tar.gz`;
-  const name = 'mlti';
+  const url = `https://github.com/dylanbr0wn/mlti/releases/download/v${version}/mlti-${platform.platform}.tar.gz`;
+  const name = `multi${platform.ext}`;
   return new Binary(name, url);
 }
 
