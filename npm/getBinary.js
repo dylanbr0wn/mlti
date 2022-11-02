c44eb14409b9824bbae22b520963c79c5b90b411
Dylan Brown <40218657+dylanbr0wn@users.noreply.github.com>
2022-11-01 23:04:15 -0700

    fix install script

--- npm/getBinary.js
+++ npm/getBinary.js
@@ -5,13 +5,18 @@ function getPlatform() {
   const type = os.type();
   const arch = os.arch();
 
-  if (type === 'Windows_NT' && arch === 'x64') return 'win64';
+  if (type === 'Windows_NT' ){
+    if (arch === 'x64') {
+			return 'win64';
+		} else {
+			return 'win32';
+		}
+  }
   if (type === 'Linux' && arch === 'x64') return 'linux';
-  if (type === 'Darwin' && arch === 'x64') return 'macos';
+  if (type === 'Darwin') return 'macos';
 
   throw new Error(`Unsupported platform: ${type} ${arch}`);
 }
-
 function getBinary() {
   const platform = getPlatform();
   const version = require('../package.json').version;
